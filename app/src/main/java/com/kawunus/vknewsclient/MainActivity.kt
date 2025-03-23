72877371149912722a5ce893a117a41d9017180a
stanislau <s.markovtsev@gmail.com>
2025-03-23 23:55:37 +0300

    Добавил верстку шапки поста

--- app/src/main/java/com/kawunus/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/kawunus/vknewsclient/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kawunus.vknewsclient.ui.PostCard
 import com.kawunus.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VkNewsClientTheme {
-
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.primary)
+                        .padding(8.dp)
+                ) {
+                    PostCard()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/kawunus/vknewsclient/ui/PostCard.kt
@@ -0,0 +1,98 @@
+package com.kawunus.vknewsclient.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kawunus.vknewsclient.R
+import com.kawunus.vknewsclient.ui.theme.VkNewsClientTheme
+
+@Composable
+fun PostCard() {
+    Card(
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.primary)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape),
+                painter = painterResource(R.drawable.placeholder_avatar),
+                contentDescription = null
+            )
+            Spacer(
+                modifier = Modifier
+                    .width(8.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(
+                    text = "/dev/null",
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+                Spacer(
+                    modifier = Modifier
+                        .height(4.dp)
+                )
+                Text(
+                    text = "14:00",
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
+            }
+            Image(
+                imageVector = Icons.Rounded.MoreVert,
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun PreviewLight() {
+    VkNewsClientTheme(
+        darkTheme = false
+    ) {
+        PostCard()
+    }
+}
+
+@Composable
+@Preview
+private fun PreviewDark() {
+    VkNewsClientTheme(
+        darkTheme = true
+    ) {
+        PostCard()
+    }
+}
