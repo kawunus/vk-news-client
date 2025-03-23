46d35cd58104652b953638d2604d3e31d0feeeee
stanislau <s.markovtsev@gmail.com>
2025-03-23 23:56:26 +0300

    Сменил Image на Icon в PostCard.kt

--- app/src/main/java/com/kawunus/vknewsclient/ui/PostCard.kt
+++ app/src/main/java/com/kawunus/vknewsclient/ui/PostCard.kt
@@ -14,13 +14,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -66,12 +66,10 @@ fun PostCard() {
                     color = MaterialTheme.colorScheme.onSecondary
                 )
             }
-            Image(
+            Icon(
                 imageVector = Icons.Rounded.MoreVert,
                 contentDescription = null,
-                colorFilter = ColorFilter.tint(
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
+                tint = MaterialTheme.colorScheme.onSecondary
             )
         }
     }
