bdebeef5489e56bdb09657d9c4171626babe5723
stanislau <s.markovtsev@gmail.com>
2025-03-24 00:31:55 +0300

    Добавил тело поста

--- app/src/main/java/com/kawunus/vknewsclient/ui/PostCard.kt
+++ app/src/main/java/com/kawunus/vknewsclient/ui/PostCard.kt
@@ -1,6 +1,7 @@
 package com.kawunus.vknewsclient.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,8 +22,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import com.kawunus.vknewsclient.R
 import com.kawunus.vknewsclient.ui.theme.VkNewsClientTheme
@@ -32,46 +35,147 @@ fun PostCard() {
     Card(
         colors = CardDefaults.cardColors(MaterialTheme.colorScheme.primary)
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
+                .padding(8.dp)
         ) {
-            Image(
+            PostCardHeader()
+            Spacer(
                 modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                painter = painterResource(R.drawable.placeholder_avatar),
-                contentDescription = null
+                    .height(8.dp)
+            )
+            PostCardBody()
+            Statistics()
+        }
+    }
+}
+
+@Composable
+private fun PostCardHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            painter = painterResource(R.drawable.placeholder_avatar),
+            contentDescription = null
+        )
+        Spacer(
+            modifier = Modifier
+                .width(8.dp)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = "/dev/null",
+                color = MaterialTheme.colorScheme.onPrimary
             )
             Spacer(
                 modifier = Modifier
-                    .width(8.dp)
+                    .height(4.dp)
             )
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                Text(
-                    text = "/dev/null",
-                    color = MaterialTheme.colorScheme.onPrimary
-                )
-                Spacer(
-                    modifier = Modifier
-                        .height(4.dp)
-                )
-                Text(
-                    text = "14:00",
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSecondary
+            Text(
+                text = "14:00",
+                color = MaterialTheme.colorScheme.onSecondary
             )
         }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Composable
+private fun PostCardBody() {
+    Text(
+        text = LoremIpsum(
+            words = 8
+        )
+            .values
+            .toList()
+            .first()
+            .toString(),
+        color = MaterialTheme.colorScheme.onPrimary
+    )
+
+    Spacer(
+        modifier = Modifier
+            .height(8.dp)
+    )
+
+    Image(
+        painter = painterResource(R.drawable.placeholder_content),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentScale = ContentScale.FillWidth
+    )
+}
+
+@Composable
+private fun Statistics() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            IconWithText(
+                iconResId = R.drawable.ic_views_count,
+                text = "916"
+            )
+        }
+        Row(
+            modifier = Modifier
+                .weight(1f),
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween
+        ) {
+            IconWithText(
+                iconResId = R.drawable.ic_share,
+                text = "7"
+            )
+            IconWithText(
+                iconResId = R.drawable.ic_comment,
+                text = "8"
+            )
+            IconWithText(
+                iconResId = R.drawable.ic_like,
+                text = "23"
+            )
+        }
+    }
+}
+
+@Composable
+private fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+        Spacer(
+            modifier = Modifier
+                .padding(4.dp)
+        )
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSecondary
+        )
     }
 }
 
